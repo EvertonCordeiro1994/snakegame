e311f62e58c880e82c6fe265730c66587225c1d1
Everton Cordeiro <evertoncordeiro@icloud.com>
2024-08-06 14:16:12 -0300

    add colisão

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
-let xSnack = 200, ySnack = 200,xFood = 0, yFood = 0, direcaoX = 0, direcaoY = 0;
+let xSnack = 200, ySnack = 200, xFood = 0, yFood = 0, direcaoX = 0, direcaoY = 0;
 
 const criarComida = () => {
-  xFood = Math.floor(Math.random()*381)
-  yFood = Math.floor(Math.random()*381)
+  xFood = Math.floor(Math.random() * 381)
+  yFood = Math.floor(Math.random() * 381)
 }
 
 function setup() {
   createCanvas(400, 400);
+  criarComida()
+
   document.querySelector('#btnEsquerda').addEventListener('click', () => {
     direcaoX = -1;
     direcaoY = 0
@@ -25,13 +27,14 @@ function setup() {
   });
 }
 
-
 function draw() {
   background('#0EA6E6');
+
   fill('#A020F0')
-  circle(100,190,9)
+  circle(xFood, yFood, 15)
+
   fill('#F80A0A');
-  square(xSnack, ySnack,15);
+  square(xSnack, ySnack, 15);
 
   xSnack += direcaoX;
   ySnack += direcaoY;
@@ -42,8 +45,11 @@ function draw() {
     ySnack = 200;
     direcaoX = 0;
     direcaoY = 0;
+    criarComida()
   }
 
-  if(xSnack === xFood && ySnack === yFood){}
-
+  if (xSnack <= xFood + 15 && xSnack + 15 >= xFood && ySnack <= yFood + 15 && ySnack + 15 >= yFood) {
+    console.log('comeu');
+    criarComida();
+  }
 }
